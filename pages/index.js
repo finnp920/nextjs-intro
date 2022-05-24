d8800f8ba20859075b4fea11a10796bcf3f10017
finnp920 <finnp920@gmail.com>
2022-05-24 22:04:07 +0900

    #1.2 Pages - #1.3 Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default (Home) => "Hi";
+import { useState } from "react";
+
+export default (Home) => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hi {counter}</h1>
+      <button onClick={() => setCounter((prev) => ++prev)}>+</button>
+    </div>
+  );
+};
