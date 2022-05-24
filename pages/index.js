782799e8a124651540b06a7f162cd7a419262388
finnp920 <finnp920@gmail.com>
2022-05-24 23:04:34 +0900

    #1.6 Custom App

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1 className="active">Hi</h1>
-      <style jsx>{`
-        a {
-          color: blue;
-        }
-      `}</style>
     </div>
   );
 }
